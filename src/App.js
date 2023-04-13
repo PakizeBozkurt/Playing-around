3b7005fd8971bf064228982f8cffe3f0c3e266de
Pakize Bozkurt <pakiozihak@gmail.com>
2023-04-13 22:54:09 +0100

    Formating

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from 'react-bootstrap';
+import Hero from './components/Hero';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
       <Footer />
     </div>
   );
