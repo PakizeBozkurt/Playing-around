1ecf4d82be038e85a73c7fcb49c6e0fd44b93db8
Pakize Bozkurt <pakiozihak@gmail.com>
2023-04-14 23:42:22 +0100

    Rendered ContactMe

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { Navbar } from "react-bootstrap";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import ContactMe from "./components/ContactMe";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <ContactMe />
       <Footer />
     </div>
   );
