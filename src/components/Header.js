693793d81841a320498d4a4728f6c5bc1cc1a89a
Pakize Bozkurt <pakiozihak@gmail.com>
2023-07-26 23:32:03 +0100

    trying to custom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,14 @@
 import React from "react";
-//images
-
 
 const Header = () => {
   return (
-    <header className="py-8">
+    
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
-        
           <button className="btn btn-sm">Work with me</button>
         </div>
       </div>
-    </header>
+  
   );
 };
 
